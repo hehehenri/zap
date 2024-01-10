4393abc13d5d1eb7662346e185e1c6d1da27ec7c
henri <henri@henr.in>
2024-01-10 03:55:13 -0300

    feat: remove room mocks and fetch rooms from server

--- apps/web/src/components/RoomList/index.tsx
+++ apps/web/src/components/RoomList/index.tsx
@@ -1,25 +1,91 @@
-import { Search } from "@/components/Search";
+"use client";
 
-const Room = () => {
+import {
+  RoomListQuery,
+  RoomListQuery$data,
+} from "@/__generated__/RoomListQuery.graphql";
+import { Search, Avatar } from "@/components";
+import { useRouter } from "next/navigation";
+import {
+  PreloadedQuery,
+  graphql,
+  usePreloadedQuery,
+  useQueryLoader,
+} from "react-relay";
+
+const roomListQuery = graphql`
+  query RoomListQuery {
+    rooms {
+      edges {
+        node {
+          id
+          participants {
+            id
+            username
+          }
+          messages {
+            id
+            content
+          }
+        }
+      }
+    }
+  }
+`;
+
+type Room = NonNullable<
+  NonNullable<
+    NonNullable<
+      NonNullable<NonNullable<RoomListQuery$data["rooms"]>>["edges"]
+    >[0]
+  >["node"]
+>;
+
+const Room = ({ room, router }: { room: Room; router }) => {
+  const handleClick = () => router.push(`/messages/${room.id}`);
+
   return (
-    <button className="flex items-center hover:bg-zinc-100/80 p-2 rounded-xl gap-2 text-left">
-      <span className="w-12 h-12 rounded-full bg-secondary-500"></span>
+    <button
+      onClick={handleClick}
+      className="flex items-center hover:bg-zinc-100/80 p-2 rounded-xl gap-2 text-left"
+    >
+      <Avatar />
       <div className="flex flex-col">
-        <span className="font-medium">username</span>
+        <span className="font-medium"></span>
         <p className="text-zinc-800">last room message preview</p>
       </div>
     </button>
   );
 };
 
-export const RoomList = () => {
+const Rooms = ({ queryRef }: { queryRef: PreloadedQuery<RoomListQuery> }) => {
+  const router = useRouter();
+  const { rooms } = usePreloadedQuery<RoomListQuery>(roomListQuery, queryRef);
+
   return (
-    <section className="min-w-64 max-w-md lg:w-[33vw] xl:w-[25vw] bg-white shadow py-2 flex flex-col gap-2 h-screen">
+    <>
+      {rooms.edges?.map((room) => {
+        // TODO: check why room is nullable
+        if (!room?.node) {
+          return "failed to load room";
+        }
+
+        return <Room room={room.node} router={router} />;
+      })}
+    </>
+  );
+};
+
+export const RoomList = () => {
+  const [queryRef] = useQueryLoader<RoomListQuery>(roomListQuery);
+
+  return (
+    <section className="min-w-64 max-w-md lg:w-[33vw] xl:w-[25vw] bg-white shadow py-2 flex flex-col gap-2 h-screen z-10">
       <div className="px-3">
         <Search />
       </div>
       <div className="h-full max-h-full flex flex-col overflow-y-auto px-3">
-        <Room />
+        {queryRef && <Rooms queryRef={queryRef} />}
       </div>
     </section>
   );
