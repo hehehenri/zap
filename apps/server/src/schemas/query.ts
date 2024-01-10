0bd6081a5994ac1ff5be2c72ce205db34c3381a3
henri <henri@henr.in>
2024-01-10 03:53:15 -0300

    feat: rooms query

--- apps/server/src/schemas/query.ts
+++ apps/server/src/schemas/query.ts
@@ -1,12 +1,14 @@
 import { GraphQLObjectType } from "graphql";
 
-import Me from '../modules/user/queries/Me';
+import Me from "../modules/user/queries/Me"
+import Rooms from "../modules/room/queries/Rooms"
 
 const query = new GraphQLObjectType({
   name: 'Query',
   description: 'Query Root',
   fields: () => ({
     me: Me,
+    rooms: Rooms,
   })
 })
 
