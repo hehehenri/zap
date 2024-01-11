57edb50c17b05e57e54ed75736a22aaf0df182af
henri <henri@henr.in>
2024-01-10 23:29:12 -0300

    fix: export env statically so I can use it without recreating it

--- apps/web/src/relay/env.ts
+++ apps/web/src/relay/env.ts
@@ -1,9 +1,11 @@
 import { Environment, Network, RecordSource, Store } from "relay-runtime";
 import { fetchFunction, subscribeFunction } from "./network";
 
-export const env = () => {
+const getEnv = () => {
   const network = Network.create(fetchFunction, subscribeFunction);
   const store = new Store(new RecordSource());
 
   return new Environment({ network, store });
 }
+
+export const env = getEnv();
