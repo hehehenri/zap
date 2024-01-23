bbce2ccd35ffb7e7172a7f7b552000ac0b3f5927
henri <henri@henr.in>
2024-01-23 05:29:18 -0300

    build: add @typescript-eslint

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,10 +1,14 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  plugins: [ "react", "react-hooks" ],
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
   root: true,
   extends: ["@repo/eslint-config/next.js", "plugin:react-hooks/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn"
+  }
 };
