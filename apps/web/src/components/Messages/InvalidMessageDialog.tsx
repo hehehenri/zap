3dffc0e85123e109a06a29cadf8d19bafe20839d
henri <henri@henr.in>
2024-01-23 10:55:35 -0300

    feat: move store message mutation logic to a separated component

---
+++ apps/web/src/components/Messages/InvalidMessageDialog.tsx
@@ -0,0 +1,32 @@
+import { Dispatch, SetStateAction, useState } from "react"
+import Dialog from "../Dialog"
+
+type Error = string | null;
+
+type Props = {
+  error: string | null,
+  setError: Dispatch<SetStateAction<Error>>,
+}
+
+export const InvalidMessageDialog = ({ error, setError }: Props) => {    
+  const open = Boolean(error);
+  const onOpenChange = (open: boolean) => {
+    if (!open) {
+      setError(null);
+    }
+  }
+  
+  return (
+    <Dialog className="max-w-sm lg:max-w-sm" title="Something went wrong" open={open} onOpenChange={onOpenChange}>
+      <section className="flex flex-col items-center gap-3">
+        <p className="w-full">{error}</p>
+        <button
+          onClick={() => onOpenChange(false)}
+          className="w-full hover:bg-secondary-100 text-secondary-400 transition rounded-2xl py-2.5 font-semibold"
+        >
+          Ok
+        </button>
+      </section>
+    </Dialog>
+  )
+}
---
+++ apps/web/src/components/Messages/StoreMessage.ts
@@ -0,0 +1,64 @@
+import { StoreMessageMutation } from "@/__generated__/StoreMessageMutation.graphql";
+import { useForm } from "react-hook-form";
+import { UseMutationConfig } from "react-relay";
+import { graphql, Disposable } from "relay-runtime";
+import z from "zod";
+
+const storeMessageSchema = z.object({
+  roomId: z.string().max(24).min(24),
+  content: z.string().max(1024, "Message exceeds the limit of 1024 characters")
+});
+
+export type StoreMessageFormData = z.infer<typeof storeMessageSchema>;
+
+export const useStoreMessageForm = ({ roomId }: {roomId: string}) => ( 
+  useForm<StoreMessageFormData>({
+    defaultValues: {
+      roomId,
+      content: "",
+    }
+  })
+);
+
+export const storeMessageMutation = graphql`
+  mutation StoreMessageMutation($input: StoreMessageInput!) {
+    storeMessage(input: $input) {
+      message {
+        id
+        content
+      }
+    }
+  }
+`;
+
+type HandlerConfig = {
+  commitMutation: (config: UseMutationConfig<StoreMessageMutation>) => Disposable, 
+  onSent: (message: string) => void,
+  onValidationError: (error: string) => void,
+};
+
+export const sendMessageHandler = ({commitMutation, onSent, onValidationError}: HandlerConfig) => (
+  async (unsafeInput: StoreMessageFormData) => {
+    const parsedInput = storeMessageSchema.safeParse(unsafeInput);
+
+    if (!parsedInput.success) {
+      const error = parsedInput.error;
+      const issue = error.issues[0];
+
+      if (issue) {
+        onValidationError(issue.message);
+      }
+
+      return;
+    }
+
+    const input = parsedInput.data;
+
+    if (!input.content) {
+      return;
+    }
+
+    commitMutation({ variables: { input } });
+    onSent(input.content);
+  }
+);
