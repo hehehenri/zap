52777e33176e2873256851b9e8599f8f54a9250b
henri <henri@henr.in>
2024-01-26 01:16:04 -0300

    test: create a query helper

--- apps/server/src/test/helpers.ts
+++ apps/server/src/test/helpers.ts
@@ -1,5 +1,7 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { database } from ".";
+import request from "supertest";
+import { createApp } from "../app";
 
 export const describeWithDb = (name: string, fn: jest.EmptyFunction) => {
   describe(name, () => {
@@ -13,4 +15,28 @@ export const describeWithDb = (name: string, fn: jest.EmptyFunction) => {
     afterEach(database.clear);
     fn();
   })
+}
+
+type Query = {
+  query: string,
+  variables: Record<any, any>  
+}
+
+export const testQuery = async (input: {
+  query: Query,
+  token?: string,
+}) => {  
+  let headers: Record<string, string> = {
+    Accept: "application/json",
+    'Content-Type': "application/json",
+  }
+
+  if (input.token) {
+    headers.Authorization = `Bearer ${input.token}`;
+  }
+
+  return await request(createApp().callback())
+    .post("/graphql")
+    .set(headers)
+    .send(JSON.stringify(input.query));
 }
