9b1994acc852834c15f565fd206df2fa60aeb0ed
henri <henri@henr.in>
2024-01-26 02:00:52 -0300

    test: update Users query test to use helpers and snapshots

--- apps/server/src/modules/user/queries/__test__/Users.spec.ts
+++ apps/server/src/modules/user/queries/__test__/Users.spec.ts
@@ -5,76 +5,43 @@ import { createApp } from "../../../../app";
 import { createUser } from "../../fixture";
 import { database } from "../../../../test";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import { describeWithDb, testQuery } from "../../../../test/helpers";
 
-describe('user/queries/users', () => {
-  let db: MongoMemoryServer;
-  
-  beforeAll(async () => {
-    db = await MongoMemoryServer.create();
-    database.connect(db) 
-  });
-  afterAll(async () => database.disconnect(db));
-  afterEach(database.clear);
-  
+const query = {
+  query: `
+    query Users {
+      users(first: 1) {
+        edges {
+          node {
+            username
+          }
+        }
+      }
+    }
+  `,
+  variables: {}
+};
+
+describeWithDb('user/queries/users', () => {  
   it("should return a collection of all users", async () => {
     const firstUser: UserDefinition = await createUser(); 
     await createUser(); 
 
     const token = generateToken(firstUser);
 
-    const payload = {
-      query: `
-        query Users {
-          users(first: 1) {
-            edges {
-              node {
-                username
-              }
-            }
-          }
-        }
-      `,
-      variables: {}
-    };
-
-    const response = await request(createApp().callback())
-      .post("/graphql")
-      .set({
-        Accept: "application/json",
-        'Content-Type': "application/json",
-        Authorization: `Bearer ${token}`
-      })
-      .send(JSON.stringify(payload));
-
-    const users: any[] = response.body.data.users.edges;
+    const response = await testQuery({ query, token });
 
     expect(response.status).toBe(200);
-    expect(users.length).toBe(1)
-    expect(users[0].node.username).toBe("user#1");
+    expect(response.body.data).toMatchSnapshot();
   });
 
   it(
     "should return unauthorized error when token is not provided",
     async () => {
-      const payload = {
-        query: `
-          query User {
-            users { __typename }
-          }
-        `,
-        variables: {}
-      };
-
-      const response = await request(createApp().callback())
-        .post("/graphql")
-        .set({
-          Accept: "application/json",
-          'Content-Type': "application/json",
-        })
-        .send(JSON.stringify(payload));
+      const response = await testQuery({ query });
 
       expect(response.status).toBe(401);
-      expect(response.body.errors[0].message).toBe("User not authorized")
+      expect(response.body.errors).toMatchSnapshot();
     });
   }
 );
